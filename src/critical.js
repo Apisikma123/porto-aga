d26a6faaaf30b85398cd7c7a09c8fbadd0a9b2b1
Apisikma123 <agaputra62@gmail.com>
2026-09-14 10:00:32 +0700

    perf(preloader): accelerate supersonic liftoff with slipstream suction wake contrail

--- src/critical.js
+++ src/critical.js
@@ -851,7 +851,7 @@ export const initPreloaderTimeline = () => {
       preloaderEl.style.display = "none";
       preloaderEl.remove();
       document.body.style.overflow = "auto";
-    }, 1800);
+    }, 1500);
   };
 
   let lastDisplayedPct = -1;
